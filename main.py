60c4bc7bf400da9b653858c79f8e0d2f1ac61966
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-30 23:52:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+import asyncio
 from pyrogram import Client
-import config
+from destinyxsparsh.database.db import sessions_col
+from config import Config
 
-app = Client(
-    "my_userbot",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    plugins=dict(root="destinyxsparsh/plugins")
-)
+async def load_all_sessions():
+    main_app = Client("main_bot", api_id=Config.API_ID, api_hash=Config.API_HASH, session_string=Config.STRING_SESSION)
+    await main_app.start()
+    print("Main Bot Started!")
+
+    # Database se saare sessions nikaalna
+    cursor = sessions_col.find({})
+    async for doc in cursor:
+        try:
+            extra_client = Client(
+                name=str(doc["user_id"]),
+                api_id=Config.API_ID,
+                api_hash=Config.API_HASH,
+                session_string=doc["session"],
+                plugins=dict(root="destinyxsparsh/plugins")
+            )
+            await extra_client.start()
+            print(f"Loaded session for user: {doc['user_id']}")
+        except Exception as e:
+            print(f"Failed to load {doc['user_id']}: {e}")
+
+    await asyncio.Event().wait() # Bot ko running rakhne ke liye
 
 if __name__ == "__main__":
-    print("Bot starting...")
-    app.run()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(load_all_sessions())
