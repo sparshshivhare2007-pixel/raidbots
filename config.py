72098254be039114f17f4e749025d87a78b1ef0f
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-31 00:02:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "12345"))
-API_HASH = os.getenv("API_HASH", "your_hash_here")
-# Agar bot token chahiye toh:
-BOT_TOKEN = os.getenv("BOT_TOKEN", None)
+class Config:
+    API_ID = int(os.getenv("API_ID", "12345"))
+    API_HASH = os.getenv("API_HASH", "your_hash")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    MONGO_URL = os.getenv("MONGO_URL")
+    OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+    START_IMG = os.getenv("START_IMG", "https://telegra.ph/file/your-image.jpg")
