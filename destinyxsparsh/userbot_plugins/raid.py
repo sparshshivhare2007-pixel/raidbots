8b05c82a3830776f312a212fc85bae03c7db3572
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-31 00:03:27 +0530

    Create raid.py

---
+++ destinyxsparsh/userbot_plugins/raid.py
@@ -0,0 +1,21 @@
+import asyncio
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("raid", ".") & filters.me)
+async def raid_cmd(client, message):
+    args = message.text.split()
+    if len(args) < 3:
+        return await message.edit("❌ **Format:** `.raid [count] [message]` (Reply to user)")
+    
+    count = int(args[1])
+    spam_msg = " ".join(args[2:])
+    
+    if not message.reply_to_message:
+        return await message.edit("❌ Please reply to a user to raid.")
+
+    target = message.reply_to_message.from_user.id
+    await message.delete()
+
+    for _ in range(count):
+        await client.send_message(target, spam_msg)
+        await asyncio.sleep(0.3)
