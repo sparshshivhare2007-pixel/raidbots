22063e81558d28e10498b9c63b904c1c99ee1358
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-31 00:02:34 +0530

    Update db.py

--- destinyxsparsh/db.py
+++ destinyxsparsh/db.py
@@ -1,12 +1,11 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import Config
 
-# Mongo setup
 client = AsyncIOMotorClient(Config.MONGO_URL)
 db = client.DestinyDB
 sessions_db = db.sessions
 
-async def add_session(user_id, session_str):
+async def add_session_to_db(user_id, session_str):
     await sessions_db.update_one(
         {"user_id": user_id},
         {"$set": {"session": session_str}},
